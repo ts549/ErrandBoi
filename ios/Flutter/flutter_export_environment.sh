aeb7821a0edc5b8f29f672fae2e9d60b7c6e5fac
Girvinn Fernandez <gdfernan@purdue.edu>
2023-01-20 22:41:15 -0500

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/girvinnfernandez/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/girvinnfernandez/errandboi/errandboi"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/girvinnfernandez/errandboi/errandboi/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/girvinnfernandez/errandboi/errandboi/.dart_tool/package_config.json"
