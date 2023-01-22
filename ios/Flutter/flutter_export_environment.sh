9fc8ffd41fba8d7b0e84f3d36da009dd011173b5
Adam Frank <adam@extrudify.io>
2023-01-22 04:01:26 -0500

    more backend stuff

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/girvinnfernandez/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/girvinnfernandez/Desktop/ErrandBoi"
+export "FLUTTER_ROOT=/Users/adamfrank/installations/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/adamfrank/Documents/Projects/boilermake/ErrandBoi"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/girvinnfernandez/Desktop/ErrandBoi/lib/errand_form.dart"
+export "FLUTTER_TARGET=/Users/adamfrank/Documents/Projects/boilermake/ErrandBoi/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/girvinnfernandez/Desktop/ErrandBoi/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/adamfrank/Documents/Projects/boilermake/ErrandBoi/.dart_tool/package_config.json"
